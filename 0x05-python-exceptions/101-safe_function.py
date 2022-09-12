062a79d24032b3b77576c9868a6a495b769b256a
PHAECKE <francisamexo@yahoo.com>
2022-09-12 06:49:00 -0700

    ..

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import sys
 
+
 def safe_function(fct, *args):
     try:
         res = fct(*args)
@@ -9,4 +10,4 @@ def safe_function(fct, *args):
         print("Exception: {}".format(e), file=sys.stderr)
         return None
     else:
-        return 
+        return res
