666d168f7203286bedf3b66e85b145df1cf9dcc1
PHAECKE <francisamexo@yahoo.com>
2022-09-21 06:41:05 -0700

    ..

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Integers addition"""
+
+
 def add_integer(a, b=98):
-    """function that add 2 numbers"""
-
-    if not a or (type(a) != int and type(a) != float):
+    """Adds two numbers
+    Args:
+        a - first number input
+        b - second number input
+    """
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-
-    return(int(a) + int(b))
+    return int(a) + int(b)
